3a56efa37477d505de8a1f31ebd62ca96be123f5
HoangVinh28 <phanthoangvinh28@gmail.com>
2023-05-20 08:37:39 +0700

    update code

--- reactjs-fe/src/App.tsx
+++ reactjs-fe/src/App.tsx
@@ -2,18 +2,17 @@ import React from "react";
 import "./App.css";
 
 import BaseWeb from "./components/ReadMore/BaseWeb";
-// import Login from "./pages/Auth/Login";
+import Login from "./pages/Auth/Login";
 
 function App() {
-  // const [isLogin, setIsLogin] = React.useState(false);
+  const [isLogin, setIsLogin] = React.useState(false);
   return (
     <>
-      {/* {isLogin ? (
+      {isLogin ? (
         <BaseWeb setIsLogin={setIsLogin} />
       ) : (
         <Login setIsLogin={setIsLogin} />
-      )} */}
-      <BaseWeb/>
+      )}
     </>
   );
 }
