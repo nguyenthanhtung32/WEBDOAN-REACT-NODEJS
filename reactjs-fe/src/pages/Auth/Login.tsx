3c7a57a66b7ab64a12fe73a293b981c2c6997a9e
HoangVinh28 <phanthoangvinh28@gmail.com>
2023-05-20 08:47:08 +0700

    up code

--- reactjs-fe/src/pages/Auth/Login.tsx
+++ reactjs-fe/src/pages/Auth/Login.tsx
@@ -1,83 +1,92 @@
 import { Button, Checkbox, Form, Input } from "antd";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import axios from "../../libraries/axiosClient";
 
 interface IProps {
-  setIsLogin: (value: boolean) => void;
-}
+    setIsLogin: (value: boolean) => void;
+  }
 
-const Login: React.FC<IProps> = (props) => {
-  const { setIsLogin } = props;
-  const [userName, setUserName] = useState<string>();
-  const [userPassWord, setUserPassWord] = useState("");
+  const Login: React.FC<IProps> = (props) => {
+    const { setIsLogin } = props;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  useEffect(() => {}, []);
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
 
-  const onSubmit = (values: any) => {
-    if (
-      (userName === "HoangVinh" && userPassWord === "Vinh02") ||
-      (userName === "ThanhTung" && userPassWord === "Tung2306")
-    ) {
-      alert("login thanh cong");
+    const token = {
+      email,
+      password,
+    };
+
+    try {
+      const response = await axios.post("/employees/login", token);
+      console.log(response);
+      
+      localStorage.setItem("token", response.data.token);
+      alert("Đăng nhập thành công");
       setIsLogin(true);
-    } else {
-      alert("login fail");
+      window.location.href = "/home";
+
+    } catch (error) {
+      console.error(error);
+      alert("Đăng nhập thất bại");
+      
     }
-    return false;
   };
 
-  const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserName(e.target.value);
-  };
-
-  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserPassWord(e.target.value);
-  };
 
   return (
-    <div style={{ padding: 28 }}>
-      <div style={{}}>
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
-          onFinish={onSubmit}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: "Please input your username!" }]}
-          >
-            <Input onChange={handleName} />
-          </Form.Item>
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Email address"
+        name="email"
+        rules={[{ required: true, message: "Please input your email!" }]}
+      >
+        <Input
+          placeholder="Enter email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          allowClear
+        />
+      </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-          >
-            <Input.Password onChange={handlePassword} />
-          </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+      </Form.Item>
 
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{ offset: 8, span: 16 }}
-          >
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{ offset: 8, span: 16 }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
 
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
-    </div>
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" onClick={(e) => handleSubmit(e)}>
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
-};
-
+}
 export default Login;
